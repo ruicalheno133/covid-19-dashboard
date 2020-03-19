596cb95d8c911e2d27dc67086199187a87dd492a
Rui Calheno <a78085@alunos.uminho.pt>
2020-03-19 00:00:08 +0000

    Updates

---
+++ precache-manifest.a3ba4b9d65100cb8670f6ad4021c6236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab17303194e3bdb6196bf65be7cfd15",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "10759151d38f48a2940e",
+    "url": "/covid-19-dashboard/static/css/main.1fddfdcf.chunk.css"
+  },
+  {
+    "revision": "f5a8f687fdad4d5d3ac4",
+    "url": "/covid-19-dashboard/static/js/2.2aebb522.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/covid-19-dashboard/static/js/2.2aebb522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10759151d38f48a2940e",
+    "url": "/covid-19-dashboard/static/js/main.fc04b95f.chunk.js"
+  },
+  {
+    "revision": "a20e42aa4ee4d158e9ef",
+    "url": "/covid-19-dashboard/static/js/runtime-main.716309f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
